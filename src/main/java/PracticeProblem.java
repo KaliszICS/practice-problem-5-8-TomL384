e29e490112fe45f11bed2e6c98e4f35ceb0de7db
TomL384 <350286454@tdsb.ca>
2026-05-07 15:48:13 +0000

    final

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,37 @@
+/**
+ * File Name: hashMap
+ * Author: Tom Leung
+ * Date Created: May 7, 2026
+ * Date Last Edited: May 7, 2026
+ */
+import java.util.HashMap;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+
+	public static HashMap <String,Integer> combineParallelArrays(String[] arr, int[] arr1){
+		HashMap <String,Integer> map = new HashMap<String, Integer>() ;
+		for (int i = 0; i < arr.length;i++){
+			map.put(arr[i], arr1[i]);
+		}
+		return map;
+	}
+
+	public static void increaseAge(HashMap <String,Integer> map, String name){		
+		int i1 = map.get(name);
+		map.put(name, i1+1);
+
+	}
+
+	public static void replaceName(HashMap <String,Integer> map, String name, String name1){		
+		int i1 = map.get(name);
+		map.remove(name);
+		map.put(name1, i1);
+
+	}
+
+
 
 }
